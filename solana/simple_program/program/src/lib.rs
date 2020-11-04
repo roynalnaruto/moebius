6f1d2f1091510cab660f837d40d27b45e10cfcf3
Rohit Narurkar <rohit.narurkar@protonmail.com>
2020-11-05 03:27:15 +0530

    feat(solana): cli for simple program

--- solana/simple_program/program/src/lib.rs
+++ solana/simple_program/program/src/lib.rs
@@ -8,6 +8,7 @@ pub mod instruction;
 pub mod processor;
 pub mod state;
 
+#[cfg(not(feature = "no-entrypoint"))]
 mod entrypoint;
 
 // Export current sdk types for downstream users building with a different sdk version
